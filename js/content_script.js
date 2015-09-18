33a295c412a83c89e6f666ebbe92c90d6dc56650
Daniel Hickman <danielhickman9@gmail.com>
2015-09-18 07:03:49 -0700

    Icons, Restructure, Select and Input, Issues

--- js/content_script.js
+++ js/content_script.js
@@ -1,1 +1,14 @@
-alert(alex('We’ve confirmed his his his his identity.').messages);
+function getSelectionText() {
+	var text = "";
+	if (window.getSelection) {
+		text = window.getSelection().toString();
+	} else if (document.selection && document.selection.type != "Control") {
+		text = document.selection.createRange().text;
+	}
+	return text;
+}
+selection = getSelectionText();
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+	sendResponse({selection: selection});
+});
---
+++ js/popup.js
@@ -0,0 +1,54 @@
+var inputEle = document.getElementById("input");
+var selection = "";
+chrome.tabs.executeScript(null, {file: "/js/alex.js"},function() {
+	chrome.tabs.executeScript(null, {file: "/js/content_script.js"},function() {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {message: "open"}, function(response) {
+				selection = response.selection;
+				if (selection.length > 0 ) {
+					inputEle.innerHTML = selection;
+					onchange();
+				} else {
+					// Don't use selection
+				};
+			});
+		});
+	});
+});
+
+
+var previousValue = undefined;
+function decorateMessage(message) {
+	var value = message.reason;
+	var index = value.indexOf('use');
+	return value.replace(/`(.+?)`/g, function ($0, $1, position) {
+		var name = position > index ? 'ok' : 'nok';
+		return '<code class="label label-' + name + '">' + $1 + '</code>';
+	});
+}
+
+function onchange() {
+	var value = document.getElementById("input").innerHTML.replace(/[\n\r]+/gi, "");
+	var messages = undefined;
+	var message = undefined;
+	if (value !== previousValue) {
+		previousValue = value;
+		messages = alex(value).messages.reverse();
+		while (issues.firstChild) {
+			issues.removeChild(issues.firstChild);
+		}
+		for (var i = messages.length - 1; i >= 0; i--) {
+			issue = document.createElement('li');
+			issue.className = 'issue';
+			issue.source = messages[i].toString();
+			issue.innerHTML = decorateMessage(messages[i]);
+			issues.appendChild(issue);
+		};
+		// 	if (report.length > 0) {
+		// 		console.log(report);
+		// 	} else {
+		// 		console.log("Your're good!");
+	}
+}
+
+inputEle.addEventListener("input", function() {onchange();}, false);
